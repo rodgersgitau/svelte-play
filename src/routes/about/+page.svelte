c8612f6940d9e17b14af4eff1a15db8207b0d9b9
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-05-12 07:45:35 +0300

    fix: responsive layout
    - adjust layout for tablets & phones
    - split `Nav` & `MobileNav` into simple components
    - add `Logo` & `Logo-Dark` images

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div class="flex flex-col gap-8 h-full w-full items-center justify-center">
-	<h1 class="w-max text-4xl">Homepage</h1>
+	<h1 class="w-max text-4xl">Aboutpage</h1>
 </div>
