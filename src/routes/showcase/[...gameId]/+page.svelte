ab3d67de018051ab6557f6549ec36870779dc18b
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-05-17 15:46:29 +0300

    feat: basic game engine
    - fix `Nav` imports
    - add basic game canvas

---
+++ src/routes/showcase/[...gameId]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import GameContainer from '$lib/game/Game.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	console.log(data);
+</script>
+
+<GameContainer />
