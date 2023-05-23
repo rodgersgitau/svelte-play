b385e37dd5aa2a42f0ed7e12cdd3535ecfe18aaf
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-05-23 09:38:20 +0300

    chore: migration to threlte
    - remove `svelte-cubed`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three', 'troika-three-text']
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
